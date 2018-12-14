7336e073630b11443f894f8e4a76ad2a46663e38
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-14 20:49:38 +0100

    Added rendering logic to BurgerIngredient component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BreadBottom, BreadTop, Seeds1, Seeds2, Meat, Cheese, Salad, Bacon} from '../../../styled/Ingredients';
+
+const BurgerIngredient = props => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ('bread-bottom'): 
+      ingredient = <BreadBottom/>;
+      break;
+    case ('bread-top'):
+      ingredient = (
+        <BreadTop>
+          <Seeds1/>
+          <Seeds2/>
+        </BreadTop>
+      )
+      break;
+    case ('meat'):
+      ingredient = <Meat/>;
+      break;
+    case ('cheese'):
+      ingredient = <Cheese/>;
+      break;
+    case ('salad'):
+      ingredient = <Salad/>;
+      break;
+    case ('bacon'):
+      ingredient = <Bacon/>;
+      break;
+    default:
+      ingredient = null;
+  }
+
+  return ingredient;
+};
+
+export default BurgerIngredient;
