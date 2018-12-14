fa0547c17352e771cdc6c2af65907b53739e07b8
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-14 19:36:07 +0100

    Added BurgerBuilder component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import BurgerBuilder from './containers/BurgerBuilder/BurderBuilder';
 import Layout from './components/Layout/Layout';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
