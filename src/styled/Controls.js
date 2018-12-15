507556d447f27b03927d90b0c7b393106f7a3009
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-15 21:42:37 +0100

    Tweaks in styling

--- src/styled/Controls.js
+++ src/styled/Controls.js
@@ -1,21 +1,12 @@
 import styled from 'styled-components';
 
-export const ControlsArea = styled.section`
-  width: 100%;
-  background-color: #cf8f2e;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  box-shadow: 0 2px 1px #ccc;
-  margin: auto;
-  padding: 10px 0;
-`;
-
 export const Control = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 5px 0;
+  padding: 7px 20px; 
+  margin: 5px;
+  color: white;
 `;
 
 export const Button = styled.button`
@@ -34,7 +25,7 @@ export const Button = styled.button`
     color: #ccc;
     cursor: default;
   }
-  
+
   &:hover:disabled {
     background-color: #AC9980;
     color: #ccc;
@@ -46,23 +37,24 @@ export const Label = styled.div`
   padding: 10px;
   font-weight: bold;
   width: 80px;
+  color: black;
 `;
 
 export const Less = styled(Control)`  
-  background-color: #D39952;
-  color: white;
-  :hover, :active { //may produce errors idk
+  cursor: pointer;
+  background-color: #d39952;
+  border: 1px solid #8f5e1e;
+  :hover, :active {
     background-color: #DAA972;
-    color: white;
   }
 `;
 
 export const More = styled(Control)`
-  background-color: #8F5E1E;
-  color: white;
-  :hover, :active { //and also here
+  cursor: pointer; 
+  background-color: #8f5e1e;
+  border: 1px solid #8f5e1e;
+  :hover, :active {
     background-color: #99703F;
-    color: white;
   }  
 `;
 
