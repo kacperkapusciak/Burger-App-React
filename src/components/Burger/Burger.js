db015afbb3b5a3babd62ff4d01f7095f26037ab3
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-15 20:06:52 +0100

    Added transforming object props to array of key-value ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Wrapper from '../../styled/BurgerWrapper';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = props => {
+
+  const ingredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map( (_,i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey}/>;
+      });
+    });
+
   return (
     <Wrapper>
       <BurgerIngredient type="bread-top"/>
