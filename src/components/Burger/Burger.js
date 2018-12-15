1828721e3b4c0921abf19f3916b8996b08fb3aba
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-15 20:16:10 +0100

    Added handling empty ingredients array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,18 +3,23 @@ import Wrapper from '../../styled/BurgerWrapper';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = props => {
 
-  const ingredients = Object.keys(props.ingredients)
+  let ingredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map( (_,i) => {
         return <BurgerIngredient key={igKey + i} type={igKey}/>;
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
 
+    if (ingredients.length === 0) {
+      ingredients = <p>Add some ingredients!</p>
+    }
   return (
     <Wrapper>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {ingredients}
       <BurgerIngredient type="bread-bottom"/>
     </Wrapper>
   );
