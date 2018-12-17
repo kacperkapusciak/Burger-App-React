6f9a17de2cfd60cadbd571485d115aa52521746f
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-17 22:58:02 +0100

    Splitted Toolbar into two components

--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -1,25 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Header = styled.header`
-  height: 56px;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: #703b09;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-  box-sizing: border-box;
-  z-index: 90;
-`;
-
-const Nav = styled.nav`
-  height: 100%;
-`;
-
+import {Header, Nav} from '../../styled/Navigation/ToolbarStyling';
 
 const toolbar = props => (
   <Header>
