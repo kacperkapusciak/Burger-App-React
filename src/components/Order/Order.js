ba3fbc0323080bc4ca1d2d3fa755733e2e10e1ce
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-05 00:20:37 +0100

    Fetching and displaying orders from firebase

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const order = props => (
-  <StyledOrder>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
-  </StyledOrder>
-);
+const order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push(
+      { 
+        name: ingredientName, 
+        amount: props.ingredients[ingredientName]
+      }
+    );
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return <span
+              style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'  
+              }} 
+              key={ig.name}>{ig.name} ({ig.amount})</span>;
+  });
+  return (
+    <StyledOrder>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </StyledOrder>
+  );
+};
 
 const StyledOrder = styled.div`
   width: 80%;
