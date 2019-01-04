66a732cf1cce1041d8b887b2b7100ee05e963ddd
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-04 23:47:23 +0100

    Added Orders page routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import BurgerBuilder from './containers/BurderBuilder';
 import Layout from './components/Layout/Layout';
 import Checkout from './containers/Checkout';
+import Orders from './containers/Orders';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
