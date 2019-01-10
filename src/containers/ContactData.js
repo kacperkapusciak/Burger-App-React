28bad077c4bb0bb129042465dcbe775555a4047c
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-11 00:10:14 +0100

    Added basic form validation

--- src/containers/ContactData.js
+++ src/containers/ContactData.js
@@ -13,7 +13,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: '' 
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false  
       },
       street: {
         elementType: 'input',
@@ -21,7 +25,13 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: '' 
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false   
       },
       zipCode: {
         elementType: 'input',
@@ -29,7 +39,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP code'
         },
-        value: '' 
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false   
       },
       country: {
         elementType: 'input',
@@ -37,7 +51,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: '' 
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false  
       },
       email: {
         elementType: 'input',
@@ -45,7 +63,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your E-mail'
         },
-        value: '' 
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false   
       },
       deliveryMethod: {
         elementType: 'select',
@@ -55,8 +77,8 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'},
           ]
         },
-        value: '' 
-      },
+        value: ''
+      }
     },
     loading: false
   }
@@ -84,6 +106,24 @@ class ContactData extends Component {
       });
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+        
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -92,7 +132,9 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm}); 
   }
 
