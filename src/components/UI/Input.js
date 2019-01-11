07ef8403a9177a18a953bd1578f7b5eba1b2695d
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-01-11 12:51:41 +0100

    Forms and form validation (#8)
    * Created custom Input component with styling
    
    * JS config setup
    
    * Bugfix
    
    * Creating inputs dynamically
    
    * Added dropdown
    
    * Handling user input
    
    * Added form submission to firebase
    
    * Added basic form validation
    
    * Bugfix
    
    * Added validation feedback
    
    * Added error messages
    
    * Added 'disabled' property to Button styled component
    
    * Handling overall form validity
    
    * Bugfixes

---
+++ src/components/UI/Input.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const input = props => {
+  let inputElement = null;
+
+  switch (props.elementType) {
+    case ('input'):
+      inputElement = <Input 
+          {...props.elementConfig} 
+          value={props.value}
+          invalid={props.invalid && props.touched}
+          onChange={props.changed}/>;
+      break;
+    case ('textarea'):
+      inputElement = <Textarea 
+          {...props.elementConfig} 
+          value={props.value}
+          invalid={props.invalid && props.touched}
+          onChange={props.changed}/>;
+      break;
+    case ('select'):
+      inputElement = (
+      <Select 
+          value={props.value} 
+          onChange={props.changed}>
+        {props.elementConfig.options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.displayValue}
+          </option>
+        ))}
+      </Select>
+      );
+      break;
+    default: 
+      inputElement = <Input 
+          {...props.elementConfig} 
+          value={props.value}
+          invalid={props.invalid && props.touched}
+          onChange={props.changed}/>;
+  }
+  let validationError = null;
+  if (props.invalid && props.touched) {
+    validationError = <ValidationError>{props.errorMessage}</ValidationError>
+  }
+  return (
+    <InputWrapper>
+      <Label>{props.label}</Label>
+      {inputElement}
+      {validationError}
+    </InputWrapper>
+  );
+
+}
+
+const InputWrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  display: block;
+  margin-bottom: 8px;
+`;
+//TODO: figure out how to do it by one styled component
+const Input = styled.input`
+  outline: none;
+  border: ${props => props.invalid ? "1px solid red" : "1px solid #ccc"};
+  background-color: ${props => props.invalid ? "#fda49a" : "white"};
+  font: inherit;
+  padding: 6px 10px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  :focus {
+    background-color: #ccc;
+    outline: none;
+  }
+`;
+const Textarea = styled.textarea`
+  outline: none;
+  border: ${props => props.invalid ? "1px solid red" : "1px solid #ccc"};
+  background-color: ${props => props.invalid ? "#fda49a" : "white"};
+  font: inherit;
+  padding: 6px 10px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  :focus {
+    background-color: #ccc;
+    outline: none;
+  }
+`;
+
+const Select = styled.select`
+  outline: none;
+  border: ${props => props.invalid ? "1px solid red" : "1px solid #ccc"};
+  background-color: ${props => props.invalid ? "#fda49a" : "white"};
+  font: inherit;
+  padding: 6px 10px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  :focus {
+    background-color: #ccc;
+    outline: none;
+  }
+`;
+const ValidationError = styled.p`
+  color: red;
+  margin: 5px 0;
+`;
+export default input;
