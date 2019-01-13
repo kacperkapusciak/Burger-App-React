7b5bb3b7846d78b243b06d127404d76b307aef27
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-13 13:36:08 +0100

    Basic redux setup

--- src/containers/BurderBuilder.js
+++ src/containers/BurderBuilder.js
@@ -26,13 +26,13 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount ( ) {
-    axios.get('https://burger-react-6775d.firebaseio.com/ingredients.json')
-      .then(response => {
-        this.setState({ingredients: response.data});
-      })
-      .catch(error => {
-        this.setState({error: true})
-      });
+    // axios.get('https://burger-react-6775d.firebaseio.com/ingredients.json')
+    //   .then(response => {
+    //     this.setState({ingredients: response.data});
+    //   })
+    //   .catch(error => {
+    //     this.setState({error: true})
+    //   });
   }
   updatePurchaseState (ingredients) {
     const sum = Object.keys(ingredients)
