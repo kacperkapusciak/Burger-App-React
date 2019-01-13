f88faf24d4a038247e8d0d62ec8557f512cbc8ba
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-13 15:05:15 +0100

    Fixed 'order now' button always-disabled bug

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../store/actions';
 class BurgerBuilder extends Component {
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -37,7 +36,7 @@ class BurgerBuilder extends Component {
         return sum + el;
       }, 0);
 
-    this.setState({purchasable: sum > 0});
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -81,7 +80,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ingredients)}
             ordered={this.purchaseHandler}
             price={this.props.price} />
         </>
