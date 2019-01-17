fb4bb241d330803252fd99b44dbcf001cc16f140
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-17 21:30:27 +0100

    Added one more order action + improvements

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../components/Order/CheckoutSummary';
@@ -15,18 +15,23 @@ class Checkout extends Component {
     this.props.history.replace('/checkout/contact-data');
   }
 
-  render() {
-    return(
-      <div>
-        <CheckoutSummary 
-          ingredients={this.props.ingredients}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}/>
-        <Route 
-          path={this.props.match.path + '/contact-data'} 
-          component={ContactData}/>  
-      </div>
-    );
+  render () {
+    let summary = <Redirect to="/"/>
+    
+    if (this.props.ingredients) {
+      summary = (
+        <div>
+          <CheckoutSummary 
+            ingredients={this.props.ingredients}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}/>
+          <Route 
+            path={this.props.match.path + '/contact-data'} 
+            component={ContactData}/>  
+        </div>
+      )
+    }
+    return summary;
   }
 }
 
--- src/containers/ContactData.js
+++ src/containers/ContactData.js
@@ -98,8 +98,7 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   }
   orderHandler = (event) => {
     event.preventDefault();
@@ -181,7 +180,7 @@ class ContactData extends Component {
         <Button success disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return(
@@ -209,14 +208,15 @@ const Wrapper = styled.div`
 const mapStateToProps = state => {
   return {
     ingredients: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: ( orderData ) => dispatch(actions.purchaseBurgerStart( orderData ))
-  }
-}
+    onOrderBurger: ( orderData ) => dispatch(actions.purchaseBurger( orderData ))
+  };
+};
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
