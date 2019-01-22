1db23eed2cf689e50726422508c71ab9984655de
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-22 15:54:28 +0100

    Closing sidedrawer after clicking a link

--- src/components/Navigation/SideDrawer.js
+++ src/components/Navigation/SideDrawer.js
@@ -8,7 +8,7 @@ const sideDrawer = (props) => {
   return (
     <>
       <Backdrop show={props.open} clicked={props.closed}/>
-      <Drawer open={props.open} >
+      <Drawer open={props.open} onClick={props.closed}>
         <Logo height="11%" marginBottom="32px"/>
         <Nav>
           <NavItems isAuth={props.isAuth}/>
