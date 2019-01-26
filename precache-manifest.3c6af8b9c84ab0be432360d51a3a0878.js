008f6de54ab38538127473425786008e683fedf3
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-01-26 16:25:34 +0100

    Updates

---
+++ precache-manifest.3c6af8b9c84ab0be432360d51a3a0878.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app-react/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "a3721e72e8309dca5790",
+    "url": "/burger-app-react/static/js/runtime~main.a3721e72.js"
+  },
+  {
+    "revision": "db03f283916653dfb026",
+    "url": "/burger-app-react/static/js/main.db03f283.chunk.js"
+  },
+  {
+    "revision": "a3f545ded7f39ed54db0",
+    "url": "/burger-app-react/static/js/4.a3f545de.chunk.js"
+  },
+  {
+    "revision": "c2e734dcba3cdb12313d",
+    "url": "/burger-app-react/static/js/3.c2e734dc.chunk.js"
+  },
+  {
+    "revision": "bd2123001cd3edd54d44",
+    "url": "/burger-app-react/static/js/2.bd212300.chunk.js"
+  },
+  {
+    "revision": "572e2239505d1204ffd2",
+    "url": "/burger-app-react/static/js/1.572e2239.chunk.js"
+  },
+  {
+    "revision": "db03f283916653dfb026",
+    "url": "/burger-app-react/static/css/main.ca04ad5a.chunk.css"
+  },
+  {
+    "revision": "89ab421d7e1822e0fbc5a48620e5017a",
+    "url": "/burger-app-react/index.html"
+  }
+];
