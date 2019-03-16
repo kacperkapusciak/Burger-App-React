f233684426a759456bb33baf09174f8b20dd90f4
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-03-16 13:49:58 +0100

    Set auth watcher

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,14 +23,9 @@ export const authFail = error => {
   };
 };
 
-export const logout = () => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('expiratonDate');
-  localStorage.removeItem('userId');
-  return {
-    type: actionTypes.AUTH_LOGOUT
-  };
-};
+export const logout = () => ({
+  type: actionTypes.AUTH_INITIATE_LOGOUT
+});
 
 export const chechAuthTimeout = expirationTime => {
   return dispatch => {
